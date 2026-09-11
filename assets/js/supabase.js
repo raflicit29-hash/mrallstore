2804552d7269f57967d99f0db43ef441eca80239
raflicit29-hash <raflicit29@gmail.com>
2026-09-12 00:01:11 +0700

    Delete assets/js directory

--- assets/js/supabase.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL="https://sjwmeoqctebgilpvfzmd.supabase.co";
-const SUPABASE_KEY="sb_publishable_0r10zRJ4HOI9bfOFZJeliw_QIqN-q4T";
-const sb=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
